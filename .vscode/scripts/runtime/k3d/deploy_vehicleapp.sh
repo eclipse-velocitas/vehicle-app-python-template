ce49ff9c4dd2f1d6394a7feaceb3203a07060342
Ebrahim.A <ebrahim.ameen@bosch.com>
2022-07-29 10:52:12 +0200

    feat: replace all hardcoded vehicleapp identifiers by the vehicle app attribute from the AppManifest.json file (#12)

--- .vscode/scripts/runtime/k3d/deploy_vehicleapp.sh
+++ .vscode/scripts/runtime/k3d/deploy_vehicleapp.sh
@@ -13,8 +13,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
 ROOT_DIRECTORY=$( realpath "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../../../.." )
+APP_NAME=$(cat $ROOT_DIRECTORY/AppManifest.json | jq .[].Name | tr -d '"')
+APP_PORT=$(cat $ROOT_DIRECTORY/AppManifest.json | jq .[].Port | tr -d '"')
+APP_REGISTRY="k3d-registry.localhost:12345"
 
 helm uninstall vapp-chart --wait
 
 # Deploy in K3D
-helm install vapp-chart $ROOT_DIRECTORY/deploy/VehicleApp/helm --values $ROOT_DIRECTORY/deploy/VehicleApp/helm/values.yaml --wait --timeout 60s --debug
+REGISTRY="k3d-registry.localhost:12345/$APP_NAME"
+helm install vapp-chart $ROOT_DIRECTORY/deploy/VehicleApp/helm \
+    --values $ROOT_DIRECTORY/deploy/VehicleApp/helm/values.yaml \
+    --set imageVehicleApp.repository="$APP_REGISTRY/$APP_NAME" \
+    --set imageVehicleApp.name=$APP_NAME \
+    --set imageVehicleApp.daprAppid=$APP_NAME \
+    --set imageVehicleApp.daprPort=$APP_PORT \
+    --wait --timeout 60s --debug
