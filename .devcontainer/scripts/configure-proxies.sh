4f83469c4636e4c64a02ff8b94584cec4b924e96
Markus Petke <MP91@users.noreply.github.com>
2022-09-16 07:04:30 +0200

    use pre-build base-image

--- .devcontainer/scripts/configure-proxies.sh
+++ .devcontainer/scripts/configure-proxies.sh
@@ -17,11 +17,17 @@ echo "#######################################################"
 echo "### Configure-proxies                               ###"
 echo "#######################################################"
 
-# this is needed for K3D to work properly
-NO_PROXY="localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,192.168.122.0/24,cattle-system.svc,.svc,.cluster.local"
+if [ "${PROXY}" = ".Proxy" ]; then
+    USE_PROXIES="true"
+    HTTP_PROXY="${PROXY_HOST}:${PROXY_PORT}"
+    HTTPS_PROXY=$HTTP_PROXY
+    FTP_PROXY=$HTTP_PROXY
+    ALL_PROXY=$HTTP_PROXY
+    NO_PROXY="localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,192.168.122.0/24,cattle-system.svc,.svc,.cluster.local"
+    CONFIGURE_GIT="true"
+fi
 
 echo "Use proxies: $USE_PROXIES"
-echo "Username: $USERNAME"
 echo "Http-proxy: $HTTP_PROXY"
 echo "Https-proxy: $HTTPS_PROXY"
 echo "Ftp-proxy: $FTP_PROXY"
@@ -35,8 +41,6 @@ if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
-
-echo "USERNAME parameter is ${USERNAME}"
 
 # Determine the appropriate non-root user
 # This recognizes the same possible user names found in Microsoft base Docker images
@@ -59,14 +63,8 @@ if [ "${USERNAME}" = "" ]; then
 fi
 echo "Selected user name is ${USERNAME}"
 
-echo "USE_PROXIES is ${USE_PROXIES}"
 if [ "${USE_PROXIES}" = "true" ]; then
     echo "Configuring proxies"
-    echo "HTTP_PROXY is ${HTTP_PROXY}"
-    echo "HTTPS_PROXY is ${HTTPS_PROXY}"
-    echo "FTP_PROXY is ${FTP_PROXY}"
-    echo "ALL_PROXY is ${ALL_PROXY}"
-    echo "NO_PROXY is ${NO_PROXY}"
 
     mkdir -p /home/${USERNAME}
     echo "export HTTP_PROXY=\"${HTTP_PROXY}\"" >> /home/${USERNAME}/.profile
@@ -80,24 +78,20 @@ if [ "${USE_PROXIES}" = "true" ]; then
     echo "export NO_PROXY=\"${NO_PROXY}\"" >> /home/${USERNAME}/.profile
     echo "export no_proxy=\"${NO_PROXY}\"" >> /home/${USERNAME}/.profile
 
-    # proxy settings to pass as VMARGS to Java
-    JAVA_HTTP_PROXY_HOST=$(echo $HTTP_PROXY | sed 's/http:\/\/\(.*\):.*/\1/')
-    JAVA_HTTP_PROXY_PORT=$(echo $HTTP_PROXY | sed 's/http:\/\/.*:\(.*\)/\1/' | tr -d '/')
-    JAVA_HTTPS_PROXY_HOST=${JAVA_HTTP_PROXY_HOST} # assume it is the same proxy - change if required
-    JAVA_HTTPS_PROXY_PORT=${JAVA_HTTP_PROXY_PORT} # assume it is the same proxy - change if required
-    JAVA_NO_PROXY=$(echo $NO_PROXY | sed 's/,/\|/g' | tr -d '[:blank:]')
-    echo "export JAVA_VMARGS_PROXIES=\"-Dhttp.proxyHost=${JAVA_HTTP_PROXY_HOST} -Dhttp.proxyPort=${JAVA_HTTP_PROXY_PORT} \
-        -Dhttps.proxyHost=${JAVA_HTTPS_PROXY_HOST} -Dhttps.proxyPort=${JAVA_HTTPS_PROXY_PORT} \
-        -Dhttp.nonProxyHosts=${JAVA_NO_PROXY}\"" >> /home/${USERNAME}/.profile
-
     # # Apply common tools proxy settings for installed tools
     if [ "${CONFIGURE_GIT}" = "true" ]; then
         su -c "git config --global http.proxy ${HTTP_PROXY}" ${USERNAME}
         su -c "git config --global https.proxy ${HTTPS_PROXY}" ${USERNAME}
-
         git config --global http.proxy ${HTTP_PROXY}
         git config --global https.proxy ${HTTPS_PROXY}
     fi
+
+    echo "# Proxy settings" >> /etc/wgetrc
+    echo "http_proxy=${HTTP_PROXY}" >> /etc/wgetrc
+    echo "https_proxy=${HTTPS_PROXY}" >> /etc/wgetrc
+    echo "ftp_proxy=${FTP_PROXY}" >> /etc/wgetrc
+    echo "no_proxy=${NO_PROXY}" >> /etc/wgetrc
+    echo "use_proxy=on" >> /etc/wgetrc
 
     # enable root user to "apt-get" via proxy
     echo "Acquire::http::proxy \"${HTTP_PROXY}\";" >> /etc/apt/apt.conf
