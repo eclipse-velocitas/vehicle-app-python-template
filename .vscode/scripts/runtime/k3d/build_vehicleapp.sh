cdfa368cf7a25f8a3a01e9054476dc283935e9c4
Markus Petke <MP91@users.noreply.github.com>
2022-11-03 10:09:55 +0100

    convert uppercase to lowercase (#62)

--- .vscode/scripts/runtime/k3d/build_vehicleapp.sh
+++ .vscode/scripts/runtime/k3d/build_vehicleapp.sh
@@ -13,7 +13,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 ROOT_DIRECTORY=$( realpath "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../../../.." )
-APP_NAME=$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq .[].Name | tr -d '"')
+APP_NAME=$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq .[].Name | tr -d '"' | tr '[:upper:]' '[:lower:]')
 DOCKERFILE_FILE="$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq .[].Dockerfile | tr -d '"')"
 
 if [ -n "$HTTP_PROXY" ]; then
