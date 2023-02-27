a453842e57baeb6056c6a03adaf52af26026ff2f
Dennis Meister <dennis.meister@bosch.com>
2023-02-27 15:43:52 +0100

    Allow current year range in license header
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- .vscode/scripts/import-example-app.sh
+++ .vscode/scripts/import-example-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022 Robert Bosch GmbH and Microsoft Corporation
+# Copyright (c) 2022-2023 Robert Bosch GmbH and Microsoft Corporation
 #
 # This program and the accompanying materials are made available under the
 # terms of the Apache License, Version 2.0 which is available at
