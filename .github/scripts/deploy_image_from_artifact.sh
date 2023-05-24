2bd89955b3d2296e0caf5c247ee5cf2cc00d8516
BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>
2023-05-24 11:57:00 +0200

    New app manifest (#143)
    * Initial changes
    
    * Fixups
    
    * Update
    
    * wip
    
    * WIP
    
    * refactor attribute
    
    * use unified package
    
    * Use camelCase for vehicleModel
    
    * Provide working version for launching and running app
    
    * Fix tasks.json
    
    * Add new-app-manifest branch for workflows
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>
    
    * Remove outdated stuff from task Runtime - Up
    
    * Update to workflow changes
    
    * Pin devenv-devcontainer-setup version
    
    * Remove lowercase conversion of app artefact
    
    * Next try for fixing artefact download
    
    * Update NOTICE-3RD-PARTY-CONTENT.md
    
    * Update tasks to call correct k3d programs
    
    * Rename runtime-up and runtime-down
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>
    
    * Sync files
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>
    
    * fix versions
    
    * fix version again
    
    ---------
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>
    Co-authored-by: Dominic Sudy <dominic.sudy@bosch.com>
    Co-authored-by: Dennis Meister <dennis.meister@bosch.com>
    Co-authored-by: Markus Petke <MP91@users.noreply.github.com>

--- .github/scripts/deploy_image_from_artifact.sh
+++ .github/scripts/deploy_image_from_artifact.sh
@@ -17,29 +17,30 @@
 set -e
 
 ROOT_DIRECTORY=$( realpath "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../.." )
-APP_NAME=$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq .[].Name | tr -d '"' | tr '[:upper:]' '[:lower:]')
-APP_PORT=$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq .[].Port | tr -d '"')
+APP_ARTIFACT_NAME=$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq .[].name | tr -d '"')
+APP_NAME_LOWERCASE=$(echo $APP_ARTIFACT_NAME | tr '[:upper:]' '[:lower:]')
+APP_PORT=50008
 APP_REGISTRY="k3d-registry.localhost:12345"
-RUNTIME_VERSION=$(cat $ROOT_DIRECTORY/.velocitas.json | jq -r '.packages[]| select(.name=="devenv-runtime-k3d")'.version)
-CONFIG_DIR="$HOME/.velocitas/packages/devenv-runtime-k3d/$RUNTIME_VERSION/src/deployment/config"
+RUNTIME_VERSION=$(cat $ROOT_DIRECTORY/.velocitas.json | jq -r '.packages[]| select(.name=="devenv-runtimes")'.version)
+HELM_CONFIG_DIR="$HOME/.velocitas/packages/devenv-runtimes/$RUNTIME_VERSION/runtime-k3d/src/app_deployment/config/helm"
 
-local_tag="$APP_REGISTRY/$APP_NAME:local"
+local_tag="$APP_REGISTRY/$APP_NAME_LOWERCASE:local"
 echo "Local URL: $local_tag"
 
-docker load -i "$APP_NAME.tar" | sed -n 's/^Loaded image ID: sha256:\([0-9a-f]*\).*/\1/p' | xargs -i docker tag {} $local_tag
+docker load -i "$APP_ARTIFACT_NAME.tar" | sed -n 's/^Loaded image ID: sha256:\([0-9a-f]*\).*/\1/p' | xargs -i docker tag {} $local_tag
 docker push $local_tag
 
-helm install vapp-chart $CONFIG_DIR/helm \
-    --values $CONFIG_DIR/helm/values.yaml \
-    --set imageVehicleApp.repository="$APP_REGISTRY/$APP_NAME" \
-    --set imageVehicleApp.name=$APP_NAME \
-    --set imageVehicleApp.daprAppid=$APP_NAME \
+helm install vapp-chart $HELM_CONFIG_DIR \
+    --values $HELM_CONFIG_DIR/values.yaml \
+    --set imageVehicleApp.repository="$APP_REGISTRY/$APP_NAME_LOWERCASE" \
+    --set imageVehicleApp.name=$APP_NAME_LOWERCASE \
+    --set imageVehicleApp.daprAppid=$APP_NAME_LOWERCASE \
     --set imageVehicleApp.daprPort=$APP_PORT \
     --wait --timeout 60s --debug
 
 kubectl get svc --all-namespaces
 kubectl get pods
 
-podname=$(kubectl get pods -o name | grep $APP_NAME)
+podname=$(kubectl get pods -o name | grep $APP_NAME_LOWERCASE)
 kubectl describe $podname
 kubectl logs $podname --all-containers
