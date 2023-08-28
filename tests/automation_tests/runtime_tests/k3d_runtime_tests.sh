2b38671a9ce02b410569356da80784f2dbab8e9d
Mladen Milutinovic <mml5bg@bosch.com>
2023-08-28 13:36:11 +0200

    Fix test for k3d runtime

--- tests/automation_tests/runtime_tests/k3d_runtime_tests.sh
+++ tests/automation_tests/runtime_tests/k3d_runtime_tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-package_name=jq ".packages[0].name" .velocitas.json
-package_version=jq ".packages[0].version" .velocitas.json
+package_name=$(jq ".packages[0].name" .velocitas.json | sed -e 's/^"//' -e 's/"$//')
+package_version=$(jq ".packages[0].version" .velocitas.json | sed -e 's/^"//' -e 's/"$//')
 
-cp /workspaces/vehicle-app-python-template/.velocitas.json "/home/vscode/.velocitas/packages/${package_name}/${package_version}/.velocitas.json"
+cp /workspaces/vehicle-app-python-template/.velocitas.json "/home/vscode/.velocitas/packages/$package_name/$package_version/.velocitas.json"
 velocitas exec runtime-k3d test
-velocitas exec runtime-k3d down
