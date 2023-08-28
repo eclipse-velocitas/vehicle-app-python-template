d1659dcde988788018aeb474d2ee723fd71d3fa1
Mladen Milutinovic <mml5bg@bosch.com>
2023-08-28 13:22:09 +0200

    Test k3d runtime tests

--- tests/automation_tests/runtime_tests/k3d_runtime_tests.sh
+++ tests/automation_tests/runtime_tests/k3d_runtime_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cp /workspaces/vehicle-app-python-template/.velocitas.json /home/vscode/.velocitas/packages/devenv-runtimes/automated-tests/.velocitas.json
+package_name=jq ".packages[0].name" .velocitas.json
+package_version=jq ".packages[0].version" .velocitas.json
+
+cp /workspaces/vehicle-app-python-template/.velocitas.json "/home/vscode/.velocitas/packages/${package_name}/${package_version}/.velocitas.json"
 velocitas exec runtime-k3d test
 velocitas exec runtime-k3d down
