0eaafe43f1dfc59a72bd0ab74ff0df1b709d69e0
Mladen Milutinovic <mml5bg@bosch.com>
2023-09-04 14:43:31 +0200

    Fix linters

--- tests/automated_tests/runtime_test.py
+++ tests/automated_tests/runtime_test.py
@@ -29,7 +29,7 @@ with open(".velocitas.json") as velocitas_file:
 
 
 class RuntimeTest(unittest.TestCase):
-    @parameterized.expand(["runtime-k3d", "runtime-kanto","runtime-local"])
+    @parameterized.expand(["runtime-k3d", "runtime-kanto", "runtime-local"])
     def test_runtime(self, runtime):
         subprocess.check_call(
             [
