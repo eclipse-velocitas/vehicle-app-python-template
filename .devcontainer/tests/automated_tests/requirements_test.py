bc1f20a7b1e5a91ffc2953eabd4922c9644ce277
Mladen <mml5bg@bosch.com>
2023-09-22 14:05:12 +0200

    Automated tests for Vehicle App Python Template (#186)

---
+++ .devcontainer/tests/automated_tests/requirements_test.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2023 Robert Bosch GmbH
+
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import unittest
+from pathlib import Path
+
+import pkg_resources
+from parameterized import parameterized
+
+
+class TestRequirements(unittest.TestCase):
+    """Test availability of required packages."""
+
+    @parameterized.expand(
+        ["./requirements.txt", "./app/requirements.txt", "./app/tests/requirements.txt"]
+    )
+    def test_requirements(self, requirement_file_path):
+        """Test that each required package is available."""
+        requirements = pkg_resources.parse_requirements(
+            Path(requirement_file_path).open()
+        )
+        for requirement in requirements:
+            requirement = str(requirement)
+            with self.subTest(requirement=requirement):
+                pkg_resources.require(requirement)
---
+++ .devcontainer/tests/automated_tests/runtime_test.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2023 Robert Bosch GmbH
+
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import subprocess  # nosec
+import unittest
+
+from parameterized import parameterized
+
+devenv_runtimes_path = (
+    subprocess.check_output(["velocitas", "package", "-p", "devenv-runtimes"])  # nosec
+    .decode("utf-8")
+    .strip("\n")
+)
+
+os.environ["VDB_PORT"] = "30555"
+os.environ["MQTT_PORT"] = "31883"
+
+
+class RuntimeTest(unittest.TestCase):
+    @parameterized.expand(["runtime_k3d", "runtime_kanto", "runtime_local"])
+    def test_runtime(self, runtime):
+        subprocess.check_call(  # nosec
+            [
+                "pytest",
+                "-s",
+                "-x",
+                (
+                    f"{devenv_runtimes_path}/{runtime}/test/integration/"
+                    f"integration_test.py::test_scripts_run_successfully"
+                ),
+            ]
+        )
+        subprocess.check_call(  # nosec
+            ["pytest", "-s", "-x", "./app/tests/integration/integration_test.py"]
+        )
+        if runtime != "runtime_local":
+            subprocess.check_call(  # nosec
+                ["velocitas", "exec", runtime.replace("_", "-"), "down"]
+            )
