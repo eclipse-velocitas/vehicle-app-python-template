a93ac065772e8a1fac5a94c553a0d4dd3ac28ba4
Mark Hüsers <mark.huesers@etas.com>
2024-05-22 11:46:14 +0200

    Add performance test app

--- app/src/main.py
+++ app/src/main.py
@@ -12,20 +12,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""A sample skeleton vehicle app."""
-
 import asyncio
 import json
 import logging
 import signal
+from datetime import datetime
 
 from vehicle import Vehicle, vehicle  # type: ignore
+from velocitas_sdk.model import DataPoint
 from velocitas_sdk.util.log import (  # type: ignore
     get_opentelemetry_log_factory,
     get_opentelemetry_log_format,
 )
 from velocitas_sdk.vdb.reply import DataPointReply
-from velocitas_sdk.vehicle_app import VehicleApp, subscribe_topic
+from velocitas_sdk.vehicle_app import VehicleApp
 
 # Configure the VehicleApp logger with the necessary log config and level.
 logging.setLogRecordFactory(get_opentelemetry_log_factory())
@@ -38,86 +38,49 @@ GET_SPEED_RESPONSE_TOPIC = "sampleapp/getSpeed/response"
 DATABROKER_SUBSCRIPTION_TOPIC = "sampleapp/currentSpeed"
 
 
-class SampleApp(VehicleApp):
+class PerformanceTestApp(VehicleApp):
     """
-    Sample skeleton vehicle app.
-
-    The skeleton subscribes to a getSpeed MQTT topic
-    to listen for incoming requests to get
-    the current vehicle speed and publishes it to
-    a response topic.
-
-    It also subcribes to the VehicleDataBroker
-    directly for updates of the
-    Vehicle.Speed signal and publishes this
-    information via another specific MQTT topic
+    A sample vehicle app that subscribes to a list of signals (from a file) and prints
+    the values + timestamps of the signals.
     """
 
     def __init__(self, vehicle_client: Vehicle):
-        # SampleApp inherits from VehicleApp.
         super().__init__()
         self.Vehicle = vehicle_client
 
     async def on_start(self):
-        """Run when the vehicle app starts"""
-        # This method will be called by the SDK when the connection to the
-        # Vehicle DataBroker is ready.
-        # Here you can subscribe for the Vehicle Signals update (e.g. Vehicle Speed).
-        await self.Vehicle.Speed.subscribe(self.on_speed_change)
+        test_signals_json = self.read_json("app/testapp_performance_subscription.json")
 
-    async def on_speed_change(self, data: DataPointReply):
-        """The on_speed_change callback, this will be executed when receiving a new
-        vehicle signal updates."""
-        # Get the current vehicle speed value from the received DatapointReply.
-        # The DatapointReply containes the values of all subscribed DataPoints of
-        # the same callback.
-        vehicle_speed = data.get(self.Vehicle.Speed).value
+        for signal_json in test_signals_json:
+            signal_str = signal_json["Signal"]
+            await self.subscribe(signal_str)
 
-        # Do anything with the received value.
-        # Example:
-        # - Publishes current speed to MQTT Topic (i.e. DATABROKER_SUBSCRIPTION_TOPIC).
-        await self.publish_event(
-            DATABROKER_SUBSCRIPTION_TOPIC,
-            json.dumps({"speed": vehicle_speed}),
-        )
+    def read_json(self, file_path: str):
+        with open(file_path, "r") as file:
+            return json.load(file)
 
-    @subscribe_topic(GET_SPEED_REQUEST_TOPIC)
-    async def on_get_speed_request_received(self, data: str) -> None:
-        """The subscribe_topic annotation is used to subscribe for incoming
-        PubSub events, e.g. MQTT event for GET_SPEED_REQUEST_TOPIC.
-        """
+    async def subscribe(self, signal_str: str):
+        try:
+            node: DataPoint = self.Vehicle.getNode(signal_str)  # type: ignore
+            await node.subscribe(self.on_node_change)
+        except AttributeError:
+            print(f"Signal {signal_str} not found in the vehicle model.")
 
-        # Use the logger with the preferred log level (e.g. debug, info, error, etc)
-        logger.debug(
-            "PubSub event for the Topic: %s -> is received with the data: %s",
-            GET_SPEED_REQUEST_TOPIC,
-            data,
-        )
+    async def on_node_change(self, data: DataPointReply):
+        current_timestamp = datetime.now()
 
-        # Getting current speed from VehicleDataBroker using the DataPoint getter.
-        vehicle_speed = (await self.Vehicle.Speed.get()).value
+        datapoint_str = data.reply.fields.popitem()[0]
+        node = self.Vehicle.getNode(datapoint_str=datapoint_str)
+        data_point_value = data.get(node).value  # type: ignore
 
-        # Do anything with the speed value.
-        # Example:
-        # - Publishes the vehicle speed to MQTT topic (i.e. GET_SPEED_RESPONSE_TOPIC).
-        await self.publish_event(
-            GET_SPEED_RESPONSE_TOPIC,
-            json.dumps(
-                {
-                    "result": {
-                        "status": 0,
-                        "message": f"""Current Speed = {vehicle_speed}""",
-                    },
-                }
-            ),
-        )
+        print(f"{current_timestamp.time()} - {node.name} - {data_point_value}")
 
 
 async def main():
     """Main function"""
     logger.info("Starting SampleApp...")
     # Constructing SampleApp and running it.
-    vehicle_app = SampleApp(vehicle)
+    vehicle_app = PerformanceTestApp(vehicle)
     await vehicle_app.run()
 
 
